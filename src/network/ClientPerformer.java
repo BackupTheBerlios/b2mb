d770308d2863b012ab9d24541804a0bb69f7ecb7
madmatt <madmatt>
2003-04-29 17:54:38 +0000

    Petite expérience qui prouve qu'on peut instaurer le dialogue entre le client et le serveur.

--- src/network/ClientPerformer.java
+++ src/network/ClientPerformer.java
@@ -3,6 +3,8 @@ package network;
 import java.net.Socket;
 import java.io.PrintStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ClientPerformer implements ClientPerformerInterface
 {
@@ -12,8 +14,15 @@ public class ClientPerformer implements ClientPerformerInterface
     {
 	try{
 	    PrintStream out = new PrintStream(socket.getOutputStream());
-	    for(int i=0; i<10; i++)
-		out.println(i+" bonjour");
+	    System.out.println("Le client lance un bonjour dans la toile");
+	    out.println("Client: bonjour");
+	    out.flush();
+	    
+	    System.out.println("Le client écoute...");
+	    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    String read = in.readLine();
+	    System.out.println("Le client se sent moins seul parce qu'on lui a répondu.");
+	    System.out.println("Cette personne a dit: "+read);
 	}
 	catch(IOException ioe)
 	    { ioe.printStackTrace(); }
--- src/network/ServerPerformer.java
+++ src/network/ServerPerformer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.Socket;
+import java.io.PrintStream;
 
 /**
  * Defines all the server's actions.
@@ -18,9 +19,15 @@ public class ServerPerformer implements ServerPerformerInterface
     {
 	try{
 	    BufferedReader r = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    String read;
-	    while((read=r.readLine())!=null)
-		System.out.println("Ce qui a été lu:\n"+read);
+	    String read=r.readLine();
+	    System.out.println("Ce qui a été lu:\n"+read);
+	    if(read.equals("Client: bonjour"))
+		System.out.println("Le serveur répond bonjour");
+	    else
+		System.out.println("Le serveur n'a pas compris ce qui a été dit, mais répond bonjour");
+	    PrintStream out = new PrintStream(socket.getOutputStream());
+	    out.println("Serveur: bonjour");
+	    out.flush();
 	}catch(IOException ioe){ ioe.printStackTrace(); }
     }
 }
