36055f8456235c7fbe4ab5385e5f32be9237a60a
madmatt <madmatt>
2003-05-16 11:04:12 +0000

    Petite expérience pour voir si on peut manipuler le client du serveur.

--- test/network/TestClient.java
+++ test/network/TestClient.java
@@ -1,19 +1,20 @@
 import network.*;
-
+import configuration.Setup;
 
 /**
  * Tests the client
  */
 public class TestClient
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws Exception
     {
 	if(args.length!=2) {
 	    System.out.println("Usage: java TestClient <hote> <port>");
 	    return;
 	}
+	Setup configuration = new Setup("config.bmb");
 	ClientPerformerInterface performer = new ClientPerformer();
-	Client client = new Client(1);
+	Client client = new Client(configuration);
 	client.startClient(args[0], Integer.parseInt(args[1]), performer);
     }
 }
--- test/network/TestServer.java
+++ test/network/TestServer.java
@@ -1,14 +1,17 @@
 import network.*;
+import configuration.Setup;
 import java.io.IOException;
 
 public class TestServer
 {
-    public static void main(String [] args) throws IOException
+    public static void main(String [] args) throws Exception
     {
-	ServerPerformerInterface performer = new ServerPerformer();
-	Server server = new Server(10, performer);
+	Setup configuration = new Setup("config.bmb");
+	ClientSystemNetworkPerformer clientSystem = new ClientSystemNetworkPerformer(configuration);
+	ServerPerformerInterface performer = new ServerPerformer(clientSystem);
+	Server server = new Server(configuration, performer);
 	System.out.println("Launching the server");
 	server.begin();
-	System.out.println("Server is finished");
+	System.out.println("Server has launched his threads");	
     }
 }
